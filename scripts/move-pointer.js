43287249ac1bf84642491caf3df9e86b479b9d8a
Dan Hickok <danhickok@gmail.com>
2026-07-24 17:05:13 -0700

    Pointer: work in progress

---
+++ scripts/move-pointer.js
@@ -0,0 +1,77 @@
+// Routines to create, show or hide, and move pointer
+
+// This keeps pointer from leaving right or bottom edge of window
+const POINTER_EDGE = 10;
+
+function establishLessonPointer() {
+  if (!window.lessonPointer) {
+    window.lessonPointer = {
+      pointerVisible: false,
+      currentX: 0,
+      currentY: 0,
+      originalCursorState: document.body.style.cursor,
+    };
+
+    const pointerElement = document.createElement('img');
+    pointerElement.id = 'lesson-pointer';
+    pointerElement.src = 'images/finger.png';
+    pointerElement.width = 200;
+    pointerElement.height = 187;
+    pointerElement.hidden = true;
+
+    window.document.body.appendChild(pointerElement);
+
+    window.document.addEventListener('mousemove', (ev) => {
+      updatePointer(ev.clientX, ev.clientY);
+      showPointer();
+      movePointer();
+      broadcastPointer();
+    });
+  }
+}
+
+function updatePointer(x, y) {
+  establishLessonPointer();
+
+  const pointerElement = document.getElementById('lesson-pointer');
+
+  window.lessonPointer.currentX = Math.min(
+    window.innerWidth - POINTER_EDGE,
+    Math.max(0, x),
+  );
+  window.lessonPointer.currentY = Math.min(
+    window.innerHeight - POINTER_EDGE,
+    Math.max(0, y),
+  );
+}
+
+function togglePointer() {
+  establishLessonPointer();
+
+  window.lessonPointer.pointerVisible = !window.lessonPointer.pointerVisible;
+  console.log(`Pointer is now ${window.lessonPointer.pointerVisible ? '' : 'in'}visible`);
+}
+
+function showPointer() {
+  establishLessonPointer();
+
+  if (window.lessonPointer.pointerVisible) {
+    window.lessonPointer.pointer.hidden = false;
+    document.body.style.cursor = 'none';
+  } else {
+    window.lessonPointer.pointer.hidden = true;
+    document.body.style.cursor = window.lessonPointer.originalCursorState;
+  }
+}
+
+function movePointer() {
+  establishLessonPointer();
+
+  if (window.lessonPointer.pointerVisible) {
+    window.lessonPointer.pointer.style.top = `${window.lessonPointer.currentY}px`;
+    window.lessonPointer.pointer.style.left = `${window.lessonPointer.currentX}px`;
+    console.log(
+      `Pointer moved to X:${window.lessonPointer.currentX} Y:${window.lessonPointer.currentY}`,
+    );
+  }
+}
---
+++ scripts/pointer-broadcaster.js
@@ -0,0 +1,37 @@
+// A set of routines to set up a BroadcastChannel object for coordinate moving a
+// pointer around on the slide in both main and speaker views
+
+function establishPointerChannel() {
+  establishLessonPointer();
+
+  if (!window.pointerChannel) {
+    window.pointerChannel = new BroadcastChannel('pointer');
+
+    window.pointerChannel.onmessage = (ev) => {
+      window.lessonPointer.pointerVisible = ev.data.visible;
+
+      const sourceWidth = ev.data.sourceWidth;
+      const sourceHeight = ev.data.sourceHeight;
+
+      const scaledX = Math.floor((ev.data.x * window.innerWidth) / sourceWidth);
+      const scaledY = Math.floor((ev.data.y * window.innerHeight) / sourceHeight);
+
+      updatePointer(scaledX, scaledY);
+      showPointer();
+      movePointer();
+    };
+  }
+}
+
+function broadcastPointer() {
+  establishLessonPointer();
+  establishPointerChannel();
+
+  window.pointerChannel.postMessage({
+    visible: window.lessonPointer.pointerVisible,
+    x: window.lessonPointer.currentX,
+    y: window.lessonPointer.currentY,
+    sourceWidth: window.innerWidth,
+    sourceHeight: window.innerHeight,
+  });
+}
