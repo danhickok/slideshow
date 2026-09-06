34b83a80840eaaec38f56e4cf0632056d446e460
Dan Hickok <danhickok@gmail.com>
2026-09-06 08:20:04 -0700

    Imported from KC

---
+++ scripts/wake-lock.mjs
@@ -0,0 +1,52 @@
+const isSupported = "wakeLock" in navigator;
+
+let wakeLockSentinel = null;
+let autoAcquire = false;
+
+export function requestWakeLock() {
+  if (!isSupported) return;
+
+  if (wakeLockSentinel != null) {
+    releaseWakeLock();
+  }
+  navigator.wakeLock
+    .request("screen")
+    .then((sentinel) => {
+      wakeLockSentinel = sentinel;
+      autoAcquire = true;
+      console.log("wait lock acquired");
+    })
+    .catch((err) => {
+      console.error(`${err.name}: ${err.message}`);
+    });
+}
+
+export function releaseWakeLock() {
+  if (!isSupported) return;
+
+  if (wakeLockSentinel !== null) {
+    wakeLockSentinel
+      .release()
+      .then(() => {
+        autoAcquire = false;
+        wakeLockSentinel = null;
+        console.log("wait lock released");
+      })
+      .catch((err) => {
+        console.error(`${err.name}: ${err.message}`);
+      });
+  }
+}
+
+if (isSupported) {
+  // if previously locked, re-acquires lock when tab gains focus
+  document.addEventListener("visibilitychange", async () => {
+    if (
+      autoAcquire &&
+      wakeLockSentinel !== null &&
+      document.visibilityState === "visible"
+    ) {
+      requestWakeLock();
+    }
+  });
+}
